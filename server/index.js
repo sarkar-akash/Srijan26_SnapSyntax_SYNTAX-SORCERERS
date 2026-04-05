4956227850f285fb7c8164362a23d9f485c7ddf1
sarkar-akash <aksark06@gmail.com>
2026-04-05 18:22:40 +0530

    remove options route causing Express error

--- server/index.js
+++ server/index.js
@@ -20,7 +20,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('/(.*)', cors(corsOptions));
 app.use(express.json());
 
 const authRouter = require('./src/routes/auth.routes');
